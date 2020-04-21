c9b11af8a67aaf09c14e9b1cf4fdab8215d36ff5
Malfeito Pina, Eugenio <emalfeito@minsait.com>
2020-04-21 18:07:24 +0200

    validaciones

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ new Vue({
 	},
 	created: function() {
 		var that = this;				
-		axios.get('http://bankentitymanagement.azurewebsites.net/api/Entity', {
+		axios.get('https://localhost:44378/api/Entity', {
 			headers: {
 				'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.LRk3GRkNBlJn0DFVo5g58l9MPAdYpCQe1sddNVmGgzY'												    
 			}
@@ -27,35 +27,60 @@ Vue.component('modal', {
 		return {
 			name: '',             
 			idEntityGroup: '',
+			idPais: '',
 			idProvince: '',
 			city: '',
 			postalCode: '',
+			adress: '',
 			telephone: '',
 			email: '',
 			logo: '',
 			active: '',
+			errorBool : true,
 			errors: [],
 		};
 	},
 	methods : {
 		submit(e) {
 			this.errors = [];	
-			if (!this.name) {						 				 
-				if (!this.name) {
+			if (!this.name || !this.idEntityGroup || !this.idProvince || !this.telephone || !this.email || !idPais) {						
+				this.errorBool = false;
+				if (!this.name && !this.errorBool) {
 					this.errors.push('El nombre es obligatorio.');
-				}
+					this.errorBool = true;
+				};
+				if (!this.idEntityGroup && !this.errorBool) {
+					this.errors.push('El grupo es obligatorio.');
+					this.errorBool = true;
+				};
+				if (!this.idPais && !this.errorBool) {
+					this.errors.push('El pais es obligatorio.');
+					this.errorBool = true;
+				};
+				if (!this.idProvince && !this.errorBool) {
+					this.errors.push('La provincia es obligatoria.');
+					this.errorBool = true;
+				};
+				if (!this.telephone && !this.errorBool) {
+					this.errors.push('El teléfono es obligatorio.');
+					this.errorBool = true;
+				};
+				if (!this.email && !this.errorBool) {
+					this.errors.push('El email es obligatorio.');
+					this.errorBool = true;
+				};				
 			}
 			else {
-				axios.post('http://bankentitymanagement.azurewebsites.net/api/Entity', { 
+				axios.post('https://localhost:44378/api/Entity', { 
 					name: this.name,
 					idEntityGroup: this.idEntityGroup,
 					idProvince: this.idProvince,
 					city: this.city,
 					postalCode: this.postalCode,
+					adress: this.adress,
 					telephone: this.telephone,
 					email: this.email,
-					logo: this.logo,
-					active: this.active,
+					logo: this.logo
 				}, {
 					headers: {						
 						'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.LRk3GRkNBlJn0DFVo5g58l9MPAdYpCQe1sddNVmGgzY'							
